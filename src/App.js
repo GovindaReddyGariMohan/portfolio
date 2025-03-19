834ea70802f8d4ace6f733235fe23d17c69ef45a
Guest2 <guest2@Yogeshs-MacBook-Air.local>
2025-03-19 11:16:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  BrowserRouter as Router,
   Route,
   Routes,
   Navigate
@@ -29,7 +28,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <div>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
@@ -43,7 +42,7 @@ function App() {
         </Routes>
         <Footer />
       </div>
-    </Router>
+    </div>
   );
 }
 
